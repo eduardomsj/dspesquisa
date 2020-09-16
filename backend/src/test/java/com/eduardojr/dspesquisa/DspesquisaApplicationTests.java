2c3fc46420c11d63fc32faac6834dedde974d42f
eduardomsj <eduardomsj@gmail.com>
2020-09-16 19:40:39 -0300

    Project created

---
+++ backend/src/test/java/com/eduardojr/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardojr.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
