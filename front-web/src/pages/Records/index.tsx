428a5bc5975fc5c62b6c2b187efd0291a428f48f
eduardomsj <eduardomsj@gmail.com>
2020-09-19 11:03:57 -0300

    Adicionando a pagina de graficos

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -3,7 +3,7 @@ import { RecordsResponse } from './types';
 import { formatDate } from './helpers';
 import axios from 'axios';
 import Pagination from './Pagination/';
-import { Link } from 'react-router-dom';
+import Filters from '../../components/Filters';
 import './styles.css';
 
 
@@ -25,13 +25,7 @@ const Records = () => {
 
     return (
         <div className="page-container">
-            <div className="filters-container records-actions">
-                <Link to="/charts">
-                    <button className="action-filters">
-                        VER GRÁFICOS
-                    </button>
-                </Link>
-            </div>
+            <Filters link="/charts" linkText="VER GRÁFICO" />
             <table className="records-table" cellPadding="0" cellSpacing="0">
                 <thead>
                     <tr>
